b0b6085fe84c76a62b79fda79f5da28deacacd02
Icer Liang <liangshuang@sutu.tech>
2024-04-05 15:38:09 +0800

    add royalty of hiya

--- src/service/royalty.ts
+++ src/service/royalty.ts
@@ -4,6 +4,8 @@ export function getRoyaltyAddress(code: string | null): string {
   if (isTestnet) {
     if (code == "test") {
       return "txch1uvsfhdev6xwgqnfwxpm32cc6r8npvcgfn2850qpsfut40j2ulmhst6h82h";
+    } else if (code == "hiya") {
+      return "txch1tanqtjx2lymsjuyy4af23p598ksnnwc8gd98zmynfraa088kewlqq62rt2";
     }
   } else {
     if (code == "hezuoshe") {
